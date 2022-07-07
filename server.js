c4333dc431d52e95db93fd69f41583698322102f
Marni1 <rotokwow@gmail.com>
2022-07-07 20:54:46 +0200

    changing dev

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const knex =require('knex')({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
-
+  client.connect();
 
 
 
