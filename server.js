4079d8964ca457f37a6b773b48d449399dd28ee3
Marni1 <rotokwow@gmail.com>
2022-07-07 20:56:05 +0200

    changing dev

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ const knex =require('knex')({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: false
     }
   });
 
