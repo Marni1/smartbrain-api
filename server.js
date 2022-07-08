cb662a56e2a04664b9ac37ef7aee5e0892dd1e82
Marni1 <rotokwow@gmail.com>
2022-07-08 16:52:05 +0200

    moze teraz

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const {Client} = require('pg')
 
 
 const db = knex({
+  connection: {
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-});
+}});
 
   
 const app = express();
