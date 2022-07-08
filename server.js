e4e3f0e253b9c95966a9158cc8775aa7b7fed50d
Marni1 <rotokwow@gmail.com>
2022-07-08 11:01:38 +0200

    nic nei dziala

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
+const knex = require('knex');
 const { response } = require('express');
 const register = require('./controlers/register');
 const signin = require('./controlers/signin');
 const profile = require ('./controlers/profile');
 const image = require('./controlers/image');
-const knex = require('knex');
+
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URI,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
@@ -28,7 +29,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {res.send('it is working');})
-app.post ('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
+app.post('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
 app.post('/register', (req, res) => {register.handleRegister(req ,res, db, bcrypt)})
 app.get('/profile/:id', (req, res)=> {profile.handleProfile(req, res, db)} )
 app.put('/image', (req,res) => {image.handleImage(req, res, db)})
