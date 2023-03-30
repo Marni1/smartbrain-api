f2f9e44b77798d54e89c1c2d65ee60cf41b089a0
Marni1 <rotokwow@gmail.com>
2023-03-30 17:24:17 +0200

    finalupdate

--- controlers/profile.js
+++ controlers/profile.js
@@ -1,22 +1,22 @@
-const handleProfile = (req, res) =>{
+const handleProfile = (req, res) => {
 
-    const {id} = req.params;
-    let found = false;
+    const { id } = req.params;
     db.select('*')
-    .from('users')
-    .where({
-        id: id,
+        .from('users')
+        .where({
+            id: id,
         })
-    .then(user => {
-        if(user.lenght){
-        res.json(user[0])
-    }
-else{
-    res.status(400).json('error getting user')
-}})
-//     if(!found) {
-//         res.status(400).json("not found")
-//     }
+        .then(user => {
+            if (user.lenght) {
+                res.json(user[0])
+            }
+            else {
+                res.status(400).json('error getting user')
+            }
+        })
+    //     if(!found) {
+    //         res.status(400).json("not found")
+    //     }
 }
 module.exports = {
     handleProfile: handleProfile
--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
 const knex = require('knex');
-const { response } = require('express');
 const register = require('./controlers/register');
 const signin = require('./controlers/signin');
-const profile = require ('./controlers/profile');
+const profile = require('./controlers/profile');
 const image = require('./controlers/image');
-const {Client} = require('pg')
+
 
 const db = knex({
   client: 'pg',
-  version: '7.2',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_DB,
   }
 });
 
 
-  
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -30,16 +31,16 @@ app.use(bodyParser.json());
 
 
 
-app.get('/', (req, res) => {res.send('it is working');})
-app.post('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
-app.post('/register', (req, res) => {register.handleRegister(req ,res, db, bcrypt)})
-app.get('/profile/:id', (req, res)=> {profile.handleProfile(req, res, db)} )
-app.put('/image', (req,res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+app.get('/', (req, res) => { res.send('it is working'); })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
